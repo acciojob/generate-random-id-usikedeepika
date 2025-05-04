dacad3bf80c7f6b7637a43343b76d43ab825d731
Usike deepika <111295716+usikedeepika@users.noreply.github.com>
2025-05-04 15:05:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
-  // write your code here
+   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+let length = 5;
+let ans = "";
+
+for (let i = 0; i < length; i++) {
+    let index = Math.floor(Math.random() * str.length); // Ensure integer index
+    let char = str.charAt(index);
+    ans += char;
+}
+
+console.log(ans); 
 }
 
 // Do not change the code below.
